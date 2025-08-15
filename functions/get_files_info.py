c3769da78a8757bb419fe50d4c5d513fe1a0a19c
ver-joakim <joakim.westblad@vermiculus.se>
2025-08-15 12:00:22 +0200

    some cleanup

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -39,9 +39,3 @@ schema_get_files_info = types.FunctionDeclaration(
         },
     ),
 )
-
-available_functions = types.Tool(
-    function_declarations=[
-        schema_get_files_info,
-    ]
-)
--- main.py
+++ main.py
@@ -5,15 +5,8 @@ from google import genai
 from google.genai import types
 from functions.get_files_info import schema_get_files_info
 
-system_prompt = """
-You are a helpful AI coding agent.
-
-When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
-
-- List files and directories
-
-All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
-"""
+from prompts import system_prompt
+from call_function import available_functions
 
 def main():
     load_dotenv()
@@ -52,22 +45,18 @@ def generate_content(client, messages, verbose):
             tools=[available_functions]
             )
     )
-    print("Response:")
-    print(response.text)
+    if not response.function_calls:
+        print("Response:")
+        print(response.text)
+        
     if response.function_calls:
       for function in response.function_calls:
           print(f"Calling function: {function.name}({function.args})")
-
 
     if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-available_functions = types.Tool(
-    function_declarations=[
-        schema_get_files_info,
-    ]
-)
 
 if __name__ == "__main__":
     main()
